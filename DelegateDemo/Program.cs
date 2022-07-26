5dae24c86ec7064c4e7b71f204bd7ec1e7d2b6f3
Sourav2707 <smartsourav33@gmail.com>
2022-07-26 17:00:36 +0530

    Delegates demo

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿using DelegateDemo;
+
+
+Console.WriteLine("Delegate Demo");
+//operation obj = new operation(Addition);   
+//Console.WriteLine("Addition is = {0}",obj.Invoke(23,27));
+MulticastDelegate1.ImplementDelegate();
+Console.ReadKey();
---
+++ EventsDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using EventsDemo;
+
+/*public delegate void DelEventHandler();*/
+//Console.WriteLine("Hello World!");
+//add += new DelEventHandler(India);
+//add += new DelEventHandler(USA);
+//add += new DelEventHandler(England);
+//add -= new DelEventHandler(USA);
+// add();
+Console.WriteLine("************Event Handling****************");
+EventHandling.ImplementEvent();
+Console.ReadKey();
